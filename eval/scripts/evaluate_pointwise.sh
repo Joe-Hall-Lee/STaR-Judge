082fcaec178da347d01c750ccbb48a87ba947f4b
Joe-Hall-Lee <sacredkayma@qq.com>
2024-09-13 23:20:04 +0800

    Fix some bugs.

--- eval/scripts/evaluate_pointwise.sh
+++ eval/scripts/evaluate_pointwise.sh
@@ -1,3 +1,3 @@
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=6
 python run_evaluate.py \
-    --model_name /H1/zhouhongli/JudgePO/LLaMA-Factory/output/Llama-3-8B-Instruct-helpsteer_simpo_2_1.6_lr_1e-6
+    --model_name /H1/zhouhongli/JudgePO/LLaMA-Factory/output/Llama-2-7b-chat-hf-judgelm_simpo_2_1.6
--- train/scripts/train_dpo.sh
+++ train/scripts/train_dpo.sh
@@ -1,3 +1,4 @@
-CUDA_VISIBLE_DEVICES="0,1,2,3" accelerate launch \
+CUDA_VISIBLE_DEVICES="6,7" accelerate launch \
     --config_file configs/accelerate/fsdp_config.yaml \
+    --main_process_port=12547 \
     src/train.py configs/llama3_full_dpo.yaml
