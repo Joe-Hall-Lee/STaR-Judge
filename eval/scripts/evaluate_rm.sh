7ad130c21e160eec3bd7f150eebb14f9608e8d12
Joe-Hall-Lee <sacredkayma@qq.com>
2024-10-17 11:23:49 +0800

    Add RewardBench evaluation.

---
+++ eval/scripts/evaluate_rm.sh
@@ -0,0 +1,6 @@
+export CUDA_VISIBLE_DEVICES="6" 
+python -m eval.rewardbench.rewardbench \
+    --model="/H1/zhouhongli/PORM/output/Llama-2-13B-chat-helpsteer_rm" \
+    --dataset="eval/data/rewardbench/filtered.json" \
+    --batch_size=16 \
+    --load_json 
