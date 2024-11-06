7aef9a30316f558c508e52cb1b462a9d2419a865
Joe-Hall-Lee <sacredkayma@qq.com>
2024-11-06 17:14:44 +0800

    Update requirements.txt.

--- PORM/scripts/evaluate_rm.sh
+++ PORM/scripts/evaluate_rm.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES="1" 
+export CUDA_VISIBLE_DEVICES="0" 
 python -m eval.rewardbench.rewardbench \
     --model="save_reward_models/Mistral-7B-Instruct-v0.3-helpsteer2_orpo_0.01_lr_1e-5_epoch_3_test_len1024_fulltrain_1e-05_datahelpsteer2_dpo.json/logs/checkpoint-156" \
     --batch_size=32 \
