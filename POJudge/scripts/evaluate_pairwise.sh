e06260fb495abe32acd4f84a95329aac03f6dd9a
Joe Hall Lee <sacredkayma@qq.com>
2024-11-17 21:21:10 +0800

    Add data/helpsteer2_et.json.

--- POJudge/scripts/evaluate_pairwise.sh
+++ POJudge/scripts/evaluate_pairwise.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=Llama-2-13b-chat_et_lr_2e-5_eval  # 任务名字
+#SBATCH --job-name=Mistral-7B-Instruct-v0.3-helpsteer2_et_lr_1e-6_eval  # 任务名字
 #SBATCH --nodes=1  # 节点
 #SBATCH --ntasks=4  # 需要多少个 cpu
 #SBATCH --gres=gpu:1  # 使用 gpu 数量，如果 2 个 gpu 写成 gpu:2
@@ -7,7 +7,7 @@
 
 export CUDA_VISIBLE_DEVICES=2
 python eval/run_bench.py \
-    --name Llama-2-13b-chat_et_lr_2e-5 \
-    --config configs/eval/llama2-13b.yaml \
+    --name Mistral-7B-Instruct-v0.3-helpsteer2_et_lr_1e-6 \
+    --config configs/eval/mistral-7b.yaml \
     --benchmarks llmbar,hhh,mtbench,biasbench,judgelm \
     --data-path ../data
--- POJudge/scripts/train_sft.sh
+++ POJudge/scripts/train_sft.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=Llama-2-13b-chat_et_lr_2e-5  # 任务名字
+#SBATCH --job-name=Mistral-7B-Instruct-v0.3_lr_1e-6  # 任务名字
 #SBATCH --nodes=1  # 节点
 #SBATCH --ntasks=4  # 需要多少个 cpu
 #SBATCH --gres=gpu:4  # 使用 gpu 数量，如果 2 个 gpu 写成 gpu:2
@@ -8,4 +8,4 @@ source ~/.bashrc
 WANDB_MODE=offline CUDA_VISIBLE_DEVICES="1,3,6,7"  accelerate launch \
     --config_file ../accelerate/fsdp_config.yaml \
     --main_process_port=12542 \
-    train/LLaMA-Factory/src/train.py configs/train/llama2_full_sft.yaml
+    train/LLaMA-Factory/src/train.py configs/train/mistral_full_sft.yaml
